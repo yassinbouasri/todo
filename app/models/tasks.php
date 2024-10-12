0488b2272bd07e564f040cd6ec9a4508eb5ee023
yassinbouasri <79885361+yassinbouasri@users.noreply.github.com>
2024-10-13 01:33:24 +0200

    Dynamically change badges color for status and priority

--- app/models/tasks.php
+++ app/models/tasks.php
@@ -16,4 +16,19 @@ class Tasks
         $stmt = $this->db->query($sql);
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
+    //changing the badge color for priority and status, according to data fetched from DB.
+    public function badge($priorityOrStatus)
+    {
+        $BadgeClasses = [
+            'high' => 'badge badge-high',
+            'medium' => 'badge badge-medium',
+            'low' => 'badge',
+            'in progress' => 'badge badge-medium',
+            'completed' => 'badge badge-success',
+            'pending' => 'badge',
+        ];
+        if (isset($BadgeClasses[strtolower($priorityOrStatus)])) {
+            return $BadgeClasses[strtolower($priorityOrStatus)];
+        }
+    }
 }
